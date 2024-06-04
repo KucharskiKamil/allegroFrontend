091fc4abd6b894465ccc8a635f7808dd476149bb
Kamil Kucharski <kamilkucharski1101@gmail.com>
2024-06-04 17:00:44 +0200

    Fix graficzny

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,32 +7,35 @@ const Form: React.FC = () => {
         <div className='form'>
             <div className='form-name'>
                 <p>Twoje imie i nazwisko</p>
-                <input type='text'></input>
-                <input type='text'></input>
+                <div className='form-name-inputs-holder'>
+                    <input type='text' placeholder='Imię'></input>
+                    <input type='text' placeholder='Nazwisko'></input>
+                </div>
+                
             </div>
             <div className='email-phone-holder'>
             <div className='form-email'>
                 <p>Twoj email</p>
-                <input type='text'></input>
+                <input type='text' placeholder='Email'></input>
             </div>
             <div className='form-phone'>
                 <p>Twoj telefon</p>
-                <input type='text'></input>
+                <input type='text' placeholder='Nr telefonu'></input>
             </div>
             </div>
             
             <div className='form-adress'>
                 <div>
                     <p>Twoj kraj</p>
-                    <input type='text'></input>
+                    <input type='text' placeholder='Kraj'></input>
                 </div>
                 <div>
                     <p>Twoje miasto</p>
-                    <input type='text'></input>
+                    <input type='text' placeholder='Miasto'></input>
                 </div>
                 <div>
-                    <p>Twoja ulica i numer domu</p>
-                    <input type='text'></input>
+                    <p>Twoja ulica i nr domu</p>
+                    <input type='text' placeholder='Ulica i nr domu'></input>
                 </div>
                 
             </div>
